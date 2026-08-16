57b0d0676595c73ba060ef8d57acb311173696b2
Erel Segal-Halevi <erelsgl@gmail.com>
2026-08-16 10:40:12 +0300

    main program

--- deepsort.py
+++ deepsort.py
@@ -5,7 +5,10 @@ def deep_sorted(x:any)->str:
 
 
 if __name__ == '__main__':
+    import doctest
+    print(doctest.testmod())
+
+    # Use this code for testing via console input-output:
     # x=eval(input())
     # print(deep_sorted(x))
-    import doctest
-    print (doctest.testmod())
+
